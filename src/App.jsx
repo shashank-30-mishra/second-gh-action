3b7b0db6c431d8d2e8525a88208cf05da436eee1
shashank mishra <shashank30mishra@gmail.com>
2023-07-17 17:25:29 +0530

    adding skip [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>Learn & Master GitHub Actions_test</h1>
+        <h1>Learn & Master GitHub Actions_test_new</h1>
       </header>
       <MainContent />
     </>
